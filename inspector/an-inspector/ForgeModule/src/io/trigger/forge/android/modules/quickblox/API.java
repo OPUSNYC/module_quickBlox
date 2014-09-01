0504271e5b4d5fda2a26d04e46a8cc917db0e3cf
unknown <samn12samn1@gmail.com>
2014-09-01 18:23:01 -0400

    added first files

---
+++ inspector/an-inspector/ForgeModule/src/io/trigger/forge/android/modules/quickblox/API.java
@@ -0,0 +1,24 @@
+package io.trigger.forge.android.modules.quickblox;
+
+import io.trigger.forge.android.core.ForgeApp;
+import io.trigger.forge.android.core.ForgeParam;
+import io.trigger.forge.android.core.ForgeTask;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+
+public class API {
+	
+	public static void videoChat(final ForgeTask task) {
+		task.performUI(new Runnable() {
+			public void run() {	    
+				Intent LaunchIntent = ForgeApp.getActivity().getPackageManager().getLaunchIntentForPackage("com.quickblox.videochatsample");
+				ForgeApp.getActivity().startActivity(LaunchIntent);
+			}
+		});
+	}
+	
+	}
+//com.quickblox.videochatsample
---
+++ module/javascript/module.js
@@ -0,0 +1,11 @@
+// Expose the native API to javascript
+forge.quickblox = {
+    showAlert: function (text, success, error) {
+        forge.internal.call('quickblox.showAlert', {text: text}, success, error);
+    }
+};
+
+// Register for our native event
+forge.internal.addEventListener("quickblox.resume", function () {
+	alert("Welcome back!");
+});
